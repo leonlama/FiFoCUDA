d0316cfe73566569db611a16a5cd74fe473108ae
leonlama <leon.lamarca@yahoo.com>
2024-09-26 17:06:46 +0200

    Add header file for TensorRT engine loader

--- include/engine_loader.h
+++ include/engine_loader.h
@@ -1,1 +1,10 @@
 // TensorRT engine loading functions
+#ifndef ENGINE_LOADER_H
+#define ENGINE_LOADER_H
+
+#include <NvInfer.h>  // TensorRT headers
+
+// Function to load a TensorRT engine from the .engine file and create an execution context
+nvinfer1::IExecutionContext* loadEngine(const char* enginePath);
+
+#endif  // ENGINE_LOADER_H
