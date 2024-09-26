f8904e7c4964ea312a3e80149b422a895f0e3d49
leonlama <leon.lamarca@yahoo.com>
2024-09-26 17:06:24 +0200

    Add header file for CUDA inference

--- include/inference.h
+++ include/inference.h
@@ -1,1 +1,16 @@
 // CUDA inference function declarations
+#ifndef INFERENCE_H
+#define INFERENCE_H
+
+#include <NvInfer.h>  // TensorRT header for IExecutionContext
+
+// Function to allocate input tensor memory in GPU VRAM
+float* allocateInputTensor(int channels, int height, int width, int batchSize);
+
+// Function to perform inference using the loaded TensorRT engine
+void runInference(nvinfer1::IExecutionContext* context, float* inputTensor, int batchSize);
+
+// Function to free the memory allocated for the input tensor
+void freeInputTensor(float* tensor);
+
+#endif  // INFERENCE_H
